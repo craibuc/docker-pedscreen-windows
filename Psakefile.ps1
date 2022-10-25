1776908638411bf951d1b245202240d2350c156c
Craig Buchanan <craig.buchanan@choa.org>
2022-10-25 12:40:46 -0500

    modifying properties adjusting build task adjusting terminal task adding params task

--- Psakefile.ps1
+++ Psakefile.ps1
@@ -1,17 +1,25 @@
 Properties {
-    $OPENJDK_TAG='11.0.13'
-    $SBT_VERSION='1.6.2'
     $APP_NAME='pedscreen-win'
     $APP_VERSION='1.2'
+
+    $SBT_VERSION='1.6.2'
+    $GIT_VERSION='2.38.1'
+    $JDK_VERSION='11.0.16.1'
+    
     $BRANCH='choa'
 }
 
 Task Build {
 	Write-Host 'Building image...'
-	docker build --build-arg "GITHUB_TOKEN=$env:GITHUB_TOKEN" --build-arg "BRANCH=$BRANCH" --tag "$APP_NAME`:$APP_VERSION" --tag "$APP_NAME`:latest" .
+    docker build --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" --build-arg "BRANCH=$BRANCH" --tag "$APP_NAME`:latest" .
 }
 
 Task Terminal {
 	Write-Host 'Starting terminal...'
-    docker run -it --rm --entrypoint pwsh "$APP_NAME`:latest"
+    docker run -it --rm "$APP_NAME`:latest" powershell
+}
+
+Task Params {
+	Write-Host 'Starting ped-screen; listing parameters...'
+    docker run -it --rm "$APP_NAME`:latest"
 }
