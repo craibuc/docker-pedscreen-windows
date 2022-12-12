752a4ff590328914b4ae4486822535d79695e32e
Craig Buchanan <craig.buchanan@choa.org>
2022-12-12 16:19:31 -0600

    correcting build task adding rebuild task adjusting params task adding publish task

--- Psakefile.ps1
+++ Psakefile.ps1
@@ -1,17 +1,27 @@
 Properties {
     $APP_NAME='pedscreen-win'
-    $APP_VERSION='1.2'
-
-    $SBT_VERSION='1.6.2'
-    $GIT_VERSION='2.38.1'
-    $JDK_VERSION='11.0.16.1'
-    
+    $APP_VERSION='1.2'    
     $BRANCH='choa'
 }
 
 Task Build {
 	Write-Host 'Building image...'
-    docker build --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" --build-arg "BRANCH=$BRANCH" --tag "$APP_NAME`:latest" .
+    docker build `
+        --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" `
+        --build-arg "BRANCH=$BRANCH" `
+        --tag "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME" `
+        --tag "$APP_NAME`:latest" `
+        .
+}
+
+Task Rebuild {
+    docker build `
+        --no-cache `
+        --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" `
+        --build-arg "BRANCH=$BRANCH" `
+        --tag "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME" `
+        --tag "$APP_NAME`:latest" `
+        .
 }
 
 Task Terminal {
@@ -21,5 +31,12 @@ Task Terminal {
 
 Task Params {
 	Write-Host 'Starting ped-screen; listing parameters...'
-    docker run -it --rm "$APP_NAME`:latest"
+    docker run -it --rm --env-file=.env "$APP_NAME`:latest"
+}
+
+Task Publish {
+	Write-Host 'Publishing image to Github...'
+	$env:GITHUB_TOKEN | docker login ghcr.io -u $env:GITHUB_ACCOUNT --password-stdin
+    docker tag $APP_NAME "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME"
+	docker push "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME`:latest"
 }
