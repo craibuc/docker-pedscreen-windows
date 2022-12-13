b34cf7757e11a4a8a860e8dea28877d9a4b592f0
Craig Buchanan <craig.buchanan@choa.org>
2022-12-13 07:49:14 -0600

    adding help text renaming BRANCH arg to GITHUB_BRANCH

--- Psakefile.ps1
+++ Psakefile.ps1
@@ -1,14 +1,51 @@
+<#
+.SYNOPSIS
+PSake is a build-automation tool written in Powershell, similar to `make` or `ant`.
+
+.EXAMPLE
+Invoke-PSake build
+
+Builds the image.
+
+.EXAMPLE
+Invoke-PSake rebuild
+
+Same as build, but will rebuild all the image's layers (--no-cache).
+
+.EXAMPLE
+Invoke-PSake terminal
+
+Start a PowerShell session in container.  Useful for diagnostic purposes.
+
+.EXAMPLE
+Invoke-PSake params
+
+Runs the container and displays the parameters that are being used by ped-screen.
+
+.EXAMPLE
+Invoke-PSake publish
+
+Publishes the image to Github.
+
+.LINK
+https://psake.readthedocs.io/en/latest/
+
+.NOTES
+To install PSake,
+
+PS> Install-Module psake
+#>
+
 Properties {
     $APP_NAME='pedscreen-win'
-    $APP_VERSION='1.2'    
-    $BRANCH='choa'
+    $APP_VERSION='1.2'
 }
 
 Task Build {
 	Write-Host 'Building image...'
     docker build `
         --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" `
-        --build-arg "BRANCH=$BRANCH" `
+        --build-arg "GITHUB_BRANCH=$( $env:GITHUB_BRANCH )" `
         --tag "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME" `
         --tag "$APP_NAME`:latest" `
         .
@@ -18,7 +55,7 @@ Task Rebuild {
     docker build `
         --no-cache `
         --build-arg "REPO_URI=https://$( $env:GITHUB_ACCOUNT )`:$( $env:GITHUB_TOKEN )@github.com/chop-dbhi/ped-screen" `
-        --build-arg "BRANCH=$BRANCH" `
+        --build-arg "GITHUB_BRANCH=$( $env:GITHUB_BRANCH )" `
         --tag "ghcr.io/$($env:GITHUB_ACCOUNT)/$APP_NAME" `
         --tag "$APP_NAME`:latest" `
         .
